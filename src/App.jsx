ac71c5104839a12a19eabfd1a89b9f693cfd9c2e
teenasen <129852390+teenasenj@users.noreply.github.com>
2023-04-25 10:54:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Purchase from './Purchase/Purchase';
 import Girlsdress from './Purchase/Girls_dress';
 import Boysdress from './Purchase/Boys_dress';
 import Newborn from './Purchase/New_born';
-import Image_upload from './file_upload/Image_upload';
+
 import './App.css';
 
 class App extends Component {
@@ -65,13 +65,6 @@ render() {
         </svg>Contact</Link>
         </li> &nbsp;&nbsp;&nbsp;
         
-        <li class="nav-item ">
-            <Link class="navbar-brand link-info" to="/Image_upload">
-            <i class="bi bi-cloud-upload-fill"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-upload-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.5 14.5V11h1v3.5a.5.5 0 0 1-1 0z"/>
-            </svg> Multiple file upload</Link>
-        </li>
         
         </ul>
         </div>
@@ -86,7 +79,7 @@ render() {
                 <Route exact path='/Girls_dress' element={< Girlsdress/>}></Route>
                 <Route exact path='/Boys_dress' element={< Boysdress />}></Route>
                 <Route exact path='/New_born' element={<Newborn />}></Route>
-                <Route exact path='/Image_upload' element={<Image_upload/>}></Route>
+         
 		</Routes>
 		</div>
 	</Router>
